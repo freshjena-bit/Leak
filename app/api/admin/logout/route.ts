2f8f416a0a7df6e7e59d65350d4c898c999e02ed
freshjena-bit <freshjena@gmail.com>
2026-06-26 15:52:21 +0700

    Update route.ts

--- app/api/admin/logout/route.ts
+++ app/api/admin/logout/route.ts
@@ -1,12 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-
-  const response = NextResponse.redirect(
+  const res = NextResponse.redirect(
     new URL("/admin/login", req.url)
   );
 
-  response.cookies.delete("admin-token");
+  res.cookies.delete("admin-token");
 
-  return response;
+  return res;
 }
