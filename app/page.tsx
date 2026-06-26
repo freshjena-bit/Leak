b8bd327e32b870f035d93d8b78bc1afe403c5a81
freshjena-bit <freshjena@gmail.com>
2026-06-26 16:10:54 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,84 +1,17 @@
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-import SearchBar from "@/components/SearchBar";
-import FileCard from "@/components/FileCard";
-import Pagination from "@/components/Pagination";
-import { adminSupabase } from "@/lib/server";
+export default function Home(){
 
-const LIMIT = 9;
+return(
 
-export default async function Home({
-  searchParams,
-}: {
-  searchParams: Promise<{
-    q?: string;
-    page?: string;
-  }>;
-}) {
-  const params = await searchParams;
+<main className="min-h-screen flex items-center justify-center">
 
-  const keyword = params.q ?? "";
+<h1 className="text-5xl font-bold">
 
-  const page = Number(params.page ?? "1");
+Legal File Share
 
-  const from = (page - 1) * LIMIT;
+</h1>
 
-  const to = from + LIMIT - 1;
+</main>
 
-  let query = adminSupabase
-    .from("files")
-    .select("*", {
-      count: "exact",
-    })
-    .eq("approved", true)
-    .order("created_at", {
-      ascending: false,
-    });
+)
 
-  if (keyword) {
-    query = query.or(
-      `title.ilike.%${keyword}%,description.ilike.%${keyword}%`
-    );
-  }
-
-  const {
-    data,
-    count,
-  } = await query.range(from, to);
-
-  const totalPage = Math.max(
-    1,
-    Math.ceil((count ?? 0) / LIMIT)
-  );
-
-  return (
-    <>
-      <Navbar />
-
-      <main className="max-w-7xl mx-auto p-5">
-
-        <SearchBar />
-
-        <div className="grid md:grid-cols-3 gap-6">
-
-          {data?.map((item) => (
-            <FileCard
-              key={item.id}
-              file={item}
-            />
-          ))}
-
-        </div>
-
-        <Pagination
-          page={page}
-          total={totalPage}
-        />
-
-      </main>
-
-      <Footer />
-
-    </>
-  );
-        }
+}
