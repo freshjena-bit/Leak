df0ae5567061c241a094bef204856fccc7dd6255
freshjena-bit <freshjena@gmail.com>
2026-06-26 15:39:51 +0700

    Create not-found.tsx

---
+++ app/not-found.tsx
@@ -0,0 +1,19 @@
+export default function NotFound() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+
+      <div className="text-center">
+
+        <h1 className="text-5xl font-bold">
+          404
+        </h1>
+
+        <p className="mt-4">
+          File tidak ditemukan.
+        </p>
+
+      </div>
+
+    </main>
+  );
+}
