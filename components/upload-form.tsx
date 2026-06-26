94d9e01bfe83043befd30e47c413eba8f215f53d
freshjena-bit <freshjena@gmail.com>
2026-06-26 16:26:18 +0700

    Create upload-form.tsx

---
+++ components/upload-form.tsx
@@ -0,0 +1,24 @@
+<form>
+
+<input name="name"/>
+
+<input name="title"/>
+
+<textarea/>
+
+<input
+type="file"
+accept="image/*"
+/>
+
+<input
+type="file"
+/>
+
+<button>
+
+Upload
+
+</button>
+
+</form>
