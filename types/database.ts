0130d8a5a9cb68991d15947ba84796eee240b9a4
freshjena-bit <freshjena@gmail.com>
2026-06-26 16:23:29 +0700

    Create database.ts

---
+++ types/database.ts
@@ -0,0 +1,21 @@
+export interface FileItem{
+
+id:string
+
+name:string
+
+title:string
+
+description:string
+
+file_url:string
+
+preview_url:string|null
+
+downloads:number
+
+approved:boolean
+
+created_at:string
+
+}
