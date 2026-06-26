b27d7ee2c52a4e89af3797fb689f369d76888c19
freshjena-bit <freshjena@gmail.com>
2026-06-26 15:40:37 +0700

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,17 @@
+export default function Footer(){
+
+return(
+
+<footer className="mt-16 border-t">
+
+<div className="max-w-6xl mx-auto py-8 text-center text-gray-500">
+
+File Share © 2026
+
+</div>
+
+</footer>
+
+)
+
+}
