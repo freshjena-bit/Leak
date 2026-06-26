8468a38ce358ad53d280e6c23ee49d5364d62e4d
freshjena-bit <freshjena@gmail.com>
2026-06-26 16:02:30 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,23 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./app/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}"
-  ],
-  theme: {
-    extend: {}
-  },
-  plugins: []
+
+content: [
+
+"./app/**/*.{ts,tsx}",
+
+"./components/**/*.{ts,tsx}"
+
+],
+
+theme: {
+
+extend: {}
+
+},
+
+plugins: []
+
 };
 
 export default config;
